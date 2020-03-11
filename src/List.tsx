1d2539c135dc53a7966799d471ac62f6f425ec32
Cody Penta <home@Codys-MacBook-Pro.local>
2020-03-11 08:47:52 -0400

    add talwing styling

--- src/List.tsx
+++ src/List.tsx
@@ -28,11 +28,36 @@ type ListProps = {
 }
 
 const List = ({ list, onRemoveItem }: ListProps) => (
-    <>
-        {list.map(item => (
-            <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-        ))}
-    </>
+    <div className="flex flex-col">
+        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                <table className="min-w-full">
+                    <thead>
+                        <tr>
+                            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Title
+                            </th>
+                            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Comments
+                            </th>
+                            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Points
+                            </th>
+                            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Actions
+                            </th>
+                            <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white">
+                        {list.map(item => (
+                            <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 )
 
 /**
@@ -54,19 +79,32 @@ export const Item = ({ item, onRemoveItem }: ItemProps) => {
     }
 
     return (
-        <>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-                <button type="button" onClick={handleRemoveItem}>
-                    Dismiss
-            </button>
-            </span>
-        </>
+        <tr>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                        <img className="h-10 w-10 rounded-full" src={`https://source.unsplash.com/user/${item.objectID}/800X600`} alt="" />
+                    </div>
+                    <div className="ml-4">
+                        <div className="text-sm leading-5 font-medium text-gray-900"><a href={item.url}>{item.title}</a></div>
+                        <div className="text-sm leading-5 text-gray-500"><span>{item.author}</span></div>
+                    </div>
+                </div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <div className="text-sm leading-5 text-gray-900">{item.num_comments}</div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <div className="text-sm leading-5 text-gray-500">{item.points}</div>
+            </td>
+            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    <button type="button" onClick={handleRemoveItem}>
+                        Dismiss
+                    </button>
+                </span>
+            </td>
+        </tr>
     );
 };
 
